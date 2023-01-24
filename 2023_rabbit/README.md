bff48ff676147c50262eac60fea1e7d5317e5559
Lanny891002 <80384226+Lanny891002@users.noreply.github.com>
2023-01-24 23:44:26 +0800

    Delete 2023_rabbit directory

--- 2023_rabbit/README.md
+++
@@ -1,4 +0,0 @@
-# 2023_rabbit
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/lanny891002/pen/rNrJZQj](https://codepen.io/lanny891002/pen/rNrJZQj).
-
--- 2023_rabbit/src/script.js
+++
@@ -1,9 +0,0 @@
-var gift1 = document.getElementById("gift1");
-var gift2 = document.getElementById("gift2");
-var gift3 = document.getElementById("gift3");
-
-function gift2_move() {
-  gift1.style.display = "none";
-  gift3.style.display = "none";
-  gift2.style.top = "60%";
-}
